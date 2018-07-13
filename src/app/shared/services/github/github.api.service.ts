de2bc6ac3e962642af4d37cb93448e0c6832a901
danielhutchinson-cw <41152067+danielhutchinson-cw@users.noreply.github.com>
2018-07-13 16:58:05 +0100

    hard coded auth token to get around rate limiting

--- src/app/shared/services/github/github.api.service.ts
+++ src/app/shared/services/github/github.api.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class GithubApiService {
     private _httpClient: HttpClient;
     private GITHUB_API_URL = 'https://api.github.com';
+    private AUTH_TOKEN = 'token efd469a0e9b97e7d57a5d263fdca8720e06dacf5';
 
     constructor(httpClient: HttpClient) {
         this._httpClient = httpClient;
     }
 
     public getUser(username: string): Observable<any> {
-        return this._httpClient.get(`${this.GITHUB_API_URL}/users/${username}`);
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Authorization': this.AUTH_TOKEN
+            })
+        };
+
+        return this._httpClient.get(`${this.GITHUB_API_URL}/users/${username}`, httpOptions);
     }
 
     public getRepositoriesForUser(username: string): Observable<any> {
-        return this._httpClient.get(`${this.GITHUB_API_URL}/users/${username}/repos`);
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Authorization': this.AUTH_TOKEN
+            })
+        };
+
+        return this._httpClient.get(`${this.GITHUB_API_URL}/users/${username}/repos`, httpOptions);
     }
 }
